9829bec9cbb95a1ce117056857d6be555d4b779b
yayuxi <s235109@dtu.dk>
2024-10-08 10:48:57 +0200

    malloc strategy tests

--- assignment_2/mm_aux.c
+++ assignment_2/mm_aux.c
@@ -1,8 +1,6 @@
 
 /* Test routines to be included at the end of mm.c */
-#include <stddef.h>  // For NULL
-#include <stdio.h>   // For printf
-#include "mm.h"      // For BlockHeader and other related definitions
+
 
 /**
  * @name    simple_macro_test
