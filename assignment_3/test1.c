38300db588076df94ff1008b60b0b62e6b3aba55
s235455@dtu.dk <s235455@dtu.dk>
2024-11-12 11:18:15 +0100

    Removed linux library

--- assignment_3/test1.c
+++ assignment_3/test1.c
@@ -4,7 +4,6 @@
 #include <assert.h>
 #include "aq.h"
 #include "aux.h"
-#include<unistd.h>
 // Define the Msg structure if it's not included in aq.h
 typedef struct {
     int value;
@@ -13,13 +12,13 @@ typedef struct {
 
 static AlarmQueue q;
 
-void *send_alarms(void *arg) {
+void *producer_send_alarms(void *arg) {
     put_alarm(q, 13);
     put_alarm(q, 14);
     return NULL;
 }
 
-void *send_normal(void *arg) {
+void *producer_send_normal(void *arg) {
     msleep(1000);
     for (int i = 1; i <= 10; i++) {
         
@@ -32,7 +31,7 @@ void *send_normal(void *arg) {
         return NULL;
     }
 
-void *receive_alarm(void *arg) {
+void *consumer(void *arg) {
     msleep(3000);
     assert(print_sizes_all(q) == 12); //Check if all 12 (normal and alarm) are sent
     assert(print_sizes(q) == 11); //Check if there are 11 normal 
@@ -54,13 +53,13 @@ int main(int argc, char **argv) {
     pthread_t t1, t2, t3;
 
     // Thread 1: Send first alarm
-    pthread_create(&t1, NULL, send_alarms, NULL);
+    pthread_create(&t1, NULL, producer_send_alarms, NULL);
 
     // Thread 2: Attempt to send second alarm, should block
-    pthread_create(&t2, NULL, send_normal, NULL);
+    pthread_create(&t2, NULL, producer_send_normal, NULL);
 
     // Thread 3: Receive the first alarm, unblocking Thread 2
-    pthread_create(&t3, NULL, receive_alarm, NULL);
+    pthread_create(&t3, NULL, consumer, NULL);
 
     // Wait for all threads to complete
     pthread_join(t1, NULL);
